868c6474a42f18e153e88dfb80593deccff84719
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2020-03-25 02:34:55 -0300

    FInished main layout

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -5,6 +5,9 @@ export class Blog {
     this.mediumImage = 'https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/68/4c/90/684c9048-c9cb-8afd-c6bb-083364' +
       '06d38f/MediumIcon-0-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-7.png/246x0w.jpg';
     this.horizontal = true;
+    this.coronavirusArticleImage = 'https://miro.medium.com/max/6408/1*F44zSvRWyD39mkOarDQjBg.png';
+    this.feignArticle = 'https://miro.medium.com/max/8064/1*HojYbuC_91aMCJO41MTtHw.jpeg';
+    this.onionArticle = 'https://miro.medium.com/max/7206/1*M1RdXI4KGq7uFTbNHbGCmQ.jpeg';
   }
 
   attached() {
--- src/home/home.js
+++ src/home/home.js
@@ -2,8 +2,7 @@ export class Home {
 
   constructor() {
     this.message = 'success';
-    this.imageLink = 'https://instagram.faep3-1.fna.fbcdn.net/vp/faf73b087aebead3f051595e179eae38/5D7E37BF/t51.2885-15' +
-      '/sh0.08/e35/s640x640/40463181_146122846326301_6667826362226144269_n.jpg?_nc_ht=instagram.faep3-1.fna.fbcdn.net';
+    this.imageLink = 'static/images/me.png';
     this.horizontal = true;
   }
 
